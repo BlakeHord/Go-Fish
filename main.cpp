e95cd8ee898d2ce39c9efef83391382117f63040
Blake Hord <blakehord@gmail.com>
2015-08-16 17:32:59 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+
+int main ()
+{
+  cout << "It's time to play...   GO FISH! *cue applause from audience*\n\nYou know the rules! Press enter to play!\n\n"
+
+  return 0;
+};
